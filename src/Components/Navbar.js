07b78ada766efad46886fb935de9a038d98a2aa7
ppanwar <ppanwar@cholachariots.com>
2024-03-04 15:36:10 +0530

    new update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -70,12 +70,12 @@ const Navbar = () => {
          fixed inset-0 z-50 bg-black bg-opacity-50 flex justify-end
           ${sidebarOpen ? 'flex' : 'hidden'}`}>
           <div className="bg-white w-64 flex flex-col p-4">
-            <a href="#" className="text-black mb-4">RESOURCES</a>
+            {/* <a href="#" className="text-black mb-4">RESOURCES</a>
             <a href="#" className="text-black mb-4">PRODUCTS</a>
             <a href="#" className="text-black mb-4">APPLICATIONS</a>
             <a href="#" className="text-black mb-4">CASE STUDIES</a>
             <button className="bg-[#2080d1] text-black px-6
-             py-3 rounded">Know More</button>
+             py-3 rounded">Know More</button> */}
           </div>
           <button className="absolute 
           top-0 right-0 m-4 text-black" onClick={toggleSidebar}>
